644744626a774e66a5f23aeddc28f27bba83db87
Duncan Alexander <hypo@hypothete.com>
2018-06-24 10:20:41 -0700

    tweaked example to load escher texture

--- example/phong-blinn.js
+++ example/phong-blinn.js
@@ -3,69 +3,65 @@ import {Shader} from '../dist/dowel.js';
 export default function PhongBlinnShader(gl) {
 
   const vert = `#version 300 es
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
+      uniform mat4 uModelViewMatrix;
+      uniform mat4 uProjectionMatrix;
 
-        in vec4 aVertexPosition;
-        in vec2 aTextureCoord;
+      in vec4 aVertexPosition;
+      in vec2 aTextureCoord;
 
-        out vec2 vTextureCoord;
-        out vec3 vVertPos;
+      out vec2 vTextureCoord;
+      out vec3 vVertPos;
 
-        void main() {
-            vTextureCoord = aTextureCoord;
-            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      void main() {
+        vTextureCoord = aTextureCoord;
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 
-            // make normals
-            vVertPos = vec4(uModelViewMatrix * aVertexPosition).xyz;
-        }
+        // make normals
+        vVertPos = vec4(uModelViewMatrix * aVertexPosition).xyz;
+      }
     `;
 
   const frag = `#version 300 es
-        #define camDir vec3(0.0, 0.0, -1.0)
+      precision mediump float;
 
-        precision mediump float;
+      uniform sampler2D uSpotMap;
+      uniform vec3 uSpotPos;
+      uniform vec3 uSpotDir;
+      uniform float uSpotLimit;
 
-        uniform sampler2D uSpotMap;
-        uniform vec3 uSpotPos;
-        uniform vec3 uSpotDir;
-        uniform float uSpotLimit;
+      in vec2 vTextureCoord;
+      in vec3 vVertPos;
+      
+      out vec4 fragColor;
 
-        in vec2 vTextureCoord;
-        in vec3 vVertPos;
-        
-        out vec4 fragColor;
+      void main() {
+        vec3 diffuse = texture(uSpotMap, vTextureCoord).rgb;
+        vec3 specColor = vec3(1.0);
 
-        void main() {
-            vec3 diffuse = vec3(0.6, 0.1, 0.1);
-            vec3 specColor = vec3(1.0, 0.3, 0.1);
+        vec3 fdx = vec3(dFdx(vVertPos.x),dFdx(vVertPos.y),dFdx(vVertPos.z));
+        vec3 fdy = vec3(dFdy(vVertPos.x),dFdy(vVertPos.y),dFdy(vVertPos.z));
+        vec3 normal = normalize(cross(fdx, fdy));
+        vec3 dirToSpot = normalize(uSpotPos - vVertPos);
 
-            vec3 fdx = vec3(dFdx(vVertPos.x),dFdx(vVertPos.y),dFdx(vVertPos.z));
-            vec3 fdy = vec3(dFdy(vVertPos.x),dFdy(vVertPos.y),dFdy(vVertPos.z));
-            vec3 normal = normalize(cross(fdx, fdy));
-            vec3 dirToSpot = normalize(uSpotPos - vVertPos);
+        float lambertian = max(dot(dirToSpot,normal), 0.0);
+        float specular = 0.0;
+        float shininess = 1.0;
+        float ambient = 0.2;
 
-            float lambertian = max(dot(dirToSpot,normal), 0.0);
-            float specular = 0.0;
-            float shininess = 1.0;
-            float ambient = 0.2;
-
-            if (lambertian > 0.0 && dot(-uSpotDir, dirToSpot) >= uSpotLimit) {
-                vec3 viewDir = normalize(-vVertPos);
-                vec3 halfDir = normalize(viewDir-uSpotDir);
-                float specAngle = max(dot(halfDir, normal), 0.0);
-                specular = pow(specAngle, shininess);
-            }
-
-            fragColor = vec4(
-                diffuse * max(lambertian, ambient) +
-                specColor * specular,
-                1.0);
-            
-            fragColor.rgb *= 0.454545;
-
-            //fragColor.rgb *= max(0.2, texture(uSpotMap, vTextureCoord).r);
+        if (lambertian > 0.0 && dot(-uSpotDir, dirToSpot) >= uSpotLimit) {
+          vec3 viewDir = normalize(-vVertPos);
+          vec3 halfDir = normalize(viewDir-uSpotDir);
+          float specAngle = max(dot(halfDir, normal), 0.0);
+          specular = pow(specAngle, shininess);
         }
+
+        fragColor = vec4(
+          diffuse * max(lambertian, ambient) +
+          specColor * specular,
+          1.0);
+        
+        fragColor.rgb *= 0.454545;
+      }
     `;
 
   const shader = new Shader(gl, vert, frag);
